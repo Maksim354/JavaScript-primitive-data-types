7007dbcd9e3b9a761fcc033d52e966797365698f
Максим Рубцов <maksim1120073@gmail.com>
2024-10-22 10:22:35 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+let radiusCircle = 5; // радіус кола
+let areaCircle = Math.PI * Math.pow(radiusCircle, 2);
+console.log("Площа кола:", areaCircle.toFixed(2)); // округлення до 2 знаків
+let length = 10;  // довжина прямокутника
+let width = 7;    // ширина прямокутника
+let areaRectangle = length * width;
+console.log("Площа прямокутника:", areaRectangle.toFixed(2)); // округлення
+let radiusCylinder = 4; // радіус циліндра
+let heightCylinder = 10; // висота циліндра
+let volumeCylinder = Math.PI * Math.pow(radiusCylinder, 2) * heightCylinder;
+console.log("Об'єм циліндра:", volumeCylinder.toFixed(2)); // округлення
